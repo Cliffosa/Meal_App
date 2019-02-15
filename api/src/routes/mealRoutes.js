43b8cab7da526247ba034b547db5ee65d8f08139
cliffosa <cliffosa002@gmail.com>
2019-02-15 07:51:54 +0100

    another update on the endpoints handler

--- api/src/routes/mealRoutes.js
+++ api/src/routes/mealRoutes.js
@@ -5,7 +5,7 @@ const router = express.Router();
 
 router.get('/api/v1/meals', mealController.getAllTodos);
 router.get('/api/v1/meals/:id', mealController.getTodo);
-router.post('/api/v1/measl', mealController.createTodo);
+router.post('/api/v1/meals', mealController.createTodo);
 router.put('/api/v1/meals/:id', mealController.updateTodo);
 router.delete('/api/v1/meals/:id', mealController.deleteTodo);
 
