d386435f954ed31e039a493cb66031d337f1d725
cliffosa <cliffosa002@gmail.com>
2019-02-15 07:49:14 +0100

    update my endpoints handlers

--- api/src/routes/mealRoutes.js
+++ api/src/routes/mealRoutes.js
@@ -3,10 +3,10 @@ import mealController from '../controllers/mealController';
 
 const router = express.Router();
 
-router.get('/api/v1/todos', mealController.getAllTodos);
-router.get('/api/v1/todos/:id', mealController.getTodo);
-router.post('/api/v1/todos', mealController.createTodo);
-router.put('/api/v1/todos/:id', mealController.updateTodo);
-router.delete('/api/v1/todos/:id', mealController.deleteTodo);
+router.get('/api/v1/meals', mealController.getAllTodos);
+router.get('/api/v1/meals/:id', mealController.getTodo);
+router.post('/api/v1/measl', mealController.createTodo);
+router.put('/api/v1/meals/:id', mealController.updateTodo);
+router.delete('/api/v1/meals/:id', mealController.deleteTodo);
 
 export default router;
