885c076608b83f33c9bec8ceca9868a3256168ad
cliffosa <cliffosa002@gmail.com>
2019-02-15 06:27:51 +0100

    Create express server

--- api/src/app.js
+++ api/src/app.js
@@ -1,1 +1,11 @@
 import express from 'express';
+
+// init epress
+const app = express();
+
+// assigned port variable
+const PORT = 5000;
+// call app to listen to the port
+app.listen(PORT, () => {
+  console.log(`App listening to port ${PORT}`);
+});
