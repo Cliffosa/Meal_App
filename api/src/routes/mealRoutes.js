f9fbcb2bb0b8195b9b105a81b825615821243da7
cliffosa <cliffosa002@gmail.com>
2019-02-15 19:28:56 +0100

    update meal routes

--- api/src/routes/mealRoutes.js
+++ api/src/routes/mealRoutes.js
@@ -4,9 +4,9 @@ import mealController from '../controllers/mealController';
 const router = express.Router();
 
 router.get('/api/v1/meals', mealController.getAllMeals);
-router.get('/api/v1/meals/:mealid', mealController.getMeal);
+router.get('/api/v1/meals/:id', mealController.getMeal);
 router.post('/api/v1/meals', mealController.createMeal);
-router.put('/api/v1/meals/:mealid', mealController.updateMeal);
-router.delete('/api/v1/meals/:mealid', mealController.deleteMeal);
+router.put('/api/v1/meals/:id', mealController.updateMeal);
+router.delete('/api/v1/meals/:id', mealController.deleteMeal);
 
 export default router;
