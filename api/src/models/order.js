de30d39b2b8bcf06653a6252b2e28645380301a1
cliffosa <cliffosa002@gmail.com>
2019-02-16 21:48:25 +0100

    add test file and order model

---
+++ api/src/models/order.js
@@ -0,0 +1,25 @@
+// import [ id ] from './models/db'
+const mealId = id;
+
+const order = [
+  {
+    orderId: 1,
+    mealId: mealId,
+    serve: 'Small',
+    price: 200
+  },
+  {
+    orderId: 2,
+    mealId: mealId,
+    serve: '3',
+    price: 300
+  },
+  {
+    orderId: 3,
+    mealId: mealId,
+    serve: '5',
+    price: 400
+  }
+];
+
+export default order;
