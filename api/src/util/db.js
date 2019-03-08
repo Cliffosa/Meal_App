1135c3f945b6a6eee996acd7b833c6848f9902ee
cliffosa <cliffosa002@gmail.com>
2019-03-08 12:21:52 +0100

    added database connection

--- api/src/util/db.js
+++ api/src/util/db.js
@@ -3,21 +3,8 @@ import { config } from 'dotenv';
 
 config();
 
-const { DATABASE_URL,  DB_NAME, DB_HOST, DB_PASSWORD, DB_PORT, DB_USER } = process.env;
-
-const sequelize = new Sequelize(DB_NAME, DB_USER, DB_PASSWORD, {
-  dialect: 'postgres',
-  url: DATABASE_URL,
-  port: DB_PORT,
-  host: DB_HOST,
-  operatorsAliases: false,
-
-  pool: {
-    max: 5,
-    min: 0,
-    acquire: 30000,
-    idle: 10000
-  }
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+  logging: false
 });
 
 export default sequelize;
