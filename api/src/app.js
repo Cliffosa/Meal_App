4345311264c0c5c071a9c16c6d2201d433f1d172
cliffosa <cliffosa002@gmail.com>
2019-03-09 05:29:02 +0100

    added api documentation for endpoints

--- api/src/app.js
+++ api/src/app.js
@@ -12,29 +12,30 @@ import Meal from './models/meals';
 import Menu from './models/menu';
 import Order from './models/orders';
 import OrderItem from './models/orderItems';
+import swaggerUi from 'swagger-ui-express';
+import swaggerDocument from './swagger.json';
 
+const app = express();
+const PORT = process.env.PORT || 8000;
 config();
-
 const client = new Client({
   connectionString: process.env.DATABASE_URL,
   ssl: true
 });
-
 client.connect();
 
-const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cors());
 app.use(router);
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+app.use('/api/v1', Routes);
 
 app.get('/', (req, res) => {
   return res.status(200).send({
     message: `Welcome To Book-A-Meal-App Book-A-Meal is an application that allows customers to make food orders and helps the food vendor know what the customers want to eat.`
   });
 });
-
-const PORT = process.env.PORT || 8000;
 
 // relationship
 User.hasMany(Order, { constraints: true, onDelete: 'CASCADE' });
